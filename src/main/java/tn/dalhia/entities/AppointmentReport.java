eff876205a3c2d6eae16caf5dbe6a4656722d7f0
ahmed-debbech <debbech.ahmed@gmail.com>
2022-02-19 23:21:06 +0100

    fix base project

---
+++ src/main/java/tn/dalhia/entities/AppointmentReport.java
@@ -0,0 +1,30 @@
+package tn.dalhia.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import tn.dalhia.entities.enumerations.AppReportCategory;
+
+@Entity
+public class AppointmentReport {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	
+	@Enumerated(EnumType.STRING)
+    private AppReportCategory category;
+	
+    private String report;
+    
+    
+
+    @OneToOne
+    @JoinColumn(name = "appointment_id")
+    private Appointment appointment;
+}

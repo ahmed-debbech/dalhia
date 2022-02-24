63e32889af26d8637ffd7a66ce47fd526ca659d2
ahmed-debbech <debbech.ahmed@gmail.com>
2022-02-24 03:56:02 +0100

    add channels controller

--- src/main/java/tn/dalhia/entities/Channel.java
+++ src/main/java/tn/dalhia/entities/Channel.java
@@ -1,13 +1,20 @@
 package tn.dalhia.entities;
 
+import lombok.*;
 import tn.dalhia.entities.enumerations.Access;
 import tn.dalhia.entities.enumerations.ChannelType;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 @Entity
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Channel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,7 +22,7 @@ public class Channel {
     private String name;
     private ChannelType channelType;
     private Access access; // private by default for INDIV chats
-    private Date dateCreated;
+    private LocalDateTime dateCreated;
     @OneToMany(cascade = CascadeType.ALL)
     private List<Message> messageList;
 

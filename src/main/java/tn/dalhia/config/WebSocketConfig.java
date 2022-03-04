bed7c86ad6a599576ba0b457b6a34d2625039139
ahmed-debbech <debbech.ahmed@gmail.com>
2022-03-05 00:18:45 +0100

    fix connection in web socket

--- src/main/java/tn/dalhia/config/WebSocketConfig.java
+++ src/main/java/tn/dalhia/config/WebSocketConfig.java
@@ -2,25 +2,28 @@ package tn.dalhia.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
     @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketMsgHandler(), "/chat");
+    }
+
+    /*@Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        //WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+        WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
         registry.addEndpoint("/socket");
                 //.setAllowedOrigins("mydomain.com").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        //WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
         registry.enableSimpleBroker("/general");
-        registry.setApplicationDestinationPrefixes("/messages");
-    }
+        registry.setApplicationDestinationPrefixes("/chat");
+    }*/
 }

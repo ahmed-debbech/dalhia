f699017d724d0ee585f37e2f6e4d3278a943f051
ahmed-debbech <debbech.ahmed@gmail.com>
2022-04-30 14:33:13 +0100

    add cors bean

--- src/main/java/tn/dalhia/controllers/TopicController.java
+++ src/main/java/tn/dalhia/controllers/TopicController.java
@@ -22,6 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/forum/topics")
 @RequiredArgsConstructor
+//@CrossOrigin("http://localhost:4200")
 @Slf4j
 public class TopicController {
 
